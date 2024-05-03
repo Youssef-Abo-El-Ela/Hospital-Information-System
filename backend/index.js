1e3938681ca13038061be835f1a20dc3ccce844c
Youssef-Abo-El-Ela <youssefmm2003@gmail.com>
2024-05-04 01:45:40 +0300

    first error on destructuring email

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,38 @@ const express = require('express')
 
 const app = express()
 
+require('dotenv').config();
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
+
 // login 
-app.post('/api/login',async (req, res, next) =>{
-  
-})
+app.post('/api/login',async (req, res) =>{
+        const {email , password} = req.body
+        const client = await pool.connect();
+        try {
+          const result = await client.query(`select u.email from users u where u.email = ${email}`);
+          if (result){
+            console.table(result.rows);
+          }
+        }
+        catch(error){
+          console.log(error);
+        }
+        finally {
+          client.release();
+        }
+      }
+)
 
 
 // register 
@@ -29,31 +57,19 @@ app.post('/api/login',async (req, res, next) =>{
 
 // })
 
-require('dotenv').config();
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
-const pool = new Pool({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: {
-    require: true,
-  },
-});
 
-async function getPgVersion() {
-    const client = await pool.connect();
-    try {
-      const result = await client.query("select * from users");
-      console.table(result.rows);
-    } finally {
-      client.release();
-    }
-  }
-getPgVersion();
+// async function getPgVersion() {
+//     const client = await pool.connect();
+//     try {
+//       const result = await client.query("select * from users");
+//       console.table(result.rows);
+//     } finally {
+//       client.release();
+//     }
+//   }
+// getPgVersion();
 
 
 app.listen(4000, ()=>{
-  console.log("server is running on port 5000")
+  console.log("server is running on port 4000")
 })
